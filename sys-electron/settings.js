e8b18cfd44606ab12cb8fa7c27ce5d845ab34cac
Andrés González <andresg9108@outlook.com>
2022-01-10 12:40:04 -0500

    Splash creation.

--- sys-electron/settings.js
+++ sys-electron/settings.js
@@ -1,23 +1,50 @@
 var oApp = {};
 
-oApp.BrowserWindow = null;
 oApp.fs = require("fs");
 oApp.url = require('url');
 oApp.path = require('path');
 oApp.electron = require('electron');
 
+oApp.BrowserWindowSplash = null;
+oApp.BrowserWindow = null;
+
+oApp.getBrowserWindowSplash = (sPath) => {
+	oApp.BrowserWindowSplash = new oApp.electron.BrowserWindow({
+		'width': 600,
+		'height': 400,
+		'center': true,
+		'frame': false,
+		'autoHideMenuBar': true,
+		'title': 'System AG',
+		'minimizable': false,
+		'resizable': false,
+		'movable': false,
+		'closable': true,
+		'skipTaskbar': true,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	oApp.BrowserWindowSplash.loadURL(oApp.url.format({
+		pathname: oApp.path.join(sPath),
+		protocol: 'file:',
+		slashes: true
+	}));
+
+	oApp.BrowserWindowSplash.on('closed', () => {
+		oApp.BrowserWindowSplash = null;
+	});
+
+	return oApp.BrowserWindowSplash;
+}
+
 oApp.getBrowserWindow = (sPath) => {
 	oApp.BrowserWindow = new oApp.electron.BrowserWindow({
-		'center': true,
-		// 'fullscreen': true,
-		// 'x': 0,
-		// 'y': 0,
+		'show': false,
 		'width': 500,
 		'height': 600,
-		// 'backgroundColor': '#000000',
-		'skipTaskbar': false,
-		'hasShadow': true,
-		//'icon': './electron-builder/icon.png',
+		'center': true,
 		'frame': true,
 		'title': 'System AG',
 		webPreferences: {
@@ -32,7 +59,7 @@ oApp.getBrowserWindow = (sPath) => {
 	}));
 
 	oApp.BrowserWindow.on('closed', () => {
-		oApp.BrowserWindow = null
+		oApp.BrowserWindow = null;
 	});
 
 	// oApp.BrowserWindow.setMenu(null);
@@ -42,4 +69,5 @@ oApp.getBrowserWindow = (sPath) => {
 	return oApp.BrowserWindow;
 }
 
+exports.getBrowserWindowSplash = oApp.getBrowserWindowSplash;
 exports.getBrowserWindow = oApp.getBrowserWindow;
